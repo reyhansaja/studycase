5f625b36ba32760e717e113ea3bf34fb86921736
reyhansaja <raihan.ts16b@gmail.com>
2025-04-15 09:02:08 +0700

    tambah menu

--- SpmDemo.java
+++ SpmDemo.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class SpmDemo {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int menu;
+        do{
+            System.out.println("========== MENU SISTEM AKADEMIK ============");
+            System.out.println("1. Tampilkan Daftar Mahasiswa");
+            System.out.println("2. Tampilkan daftar mata kuliah");
+            System.out.println("3. Tampilkan data Penilaian");
+            System.out.println("4. Urutkan Mahasiswa berdasarkan Nilai Akhir");
+            System.out.println("5. Cari Mahasiswa berdasarkan NIM");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih Menu:");
+            menu = input.nextInt();
+
+            switch (menu) {
+                case 1:
+                    System.out.println("Menampilkan daftar mahasiswa...");
+                    break;
+                case 2:
+                    System.out.println("Menampilkan daftar mata kuliah...");
+                    break;
+                case 3:
+                    System.out.println("Menampilkan data penilaian...");
+                    break;
+                case 4:
+                    System.out.println("Mengurutkan mahasiswa berdasarkan nilai akhir...");
+                    break;
+                case 5:
+                    System.out.println("Mencari mahasiswa berdasarkan NIM...");
+                    // Logika pencarian berdasarkan NIM di sini
+                    break;
+                case 0:
+                    System.out.println("Keluar dari program.");
+                    break;
+                default:
+                    System.out.println("Menu tidak valid, silakan coba lagi.");
+            }
+    
+            System.out.println();
         
+        } while (menu != 0);
     }
 }
